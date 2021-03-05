493a183d1e2abd76501c40138b1db960834784d2
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-03-05 14:04:19 -0800

    Initial state

--- src/context.js
+++ src/context.js
@@ -1,13 +1,20 @@
 import React, { useState, useContext, useReducer, useEffect } from "react";
 import cartItems from "./data";
 import reducer from "./reducer";
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
 const url = "https://course-api.com/react-useReducer-cart-project";
 const AppContext = React.createContext();
 
+const initialState = {
+  loading: false,
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+}
+
+
 const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState(cartItems);
+  const [state, dispatch] = useReducer(initialState);
 
   return (
     <AppContext.Provider
@@ -19,7 +26,7 @@ const AppProvider = ({ children }) => {
     </AppContext.Provider>
   );
 };
-// make sure use
+// Custom hook to use context
 export const useGlobalContext = () => {
   return useContext(AppContext);
 };
