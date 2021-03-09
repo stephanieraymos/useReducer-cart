7f1ea0c936ee36edf6316a7b16c14e8471f2fd55
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-03-08 16:57:29 -0800

    Increase and decrease functionality

--- src/reducer.js
+++ src/reducer.js
@@ -22,6 +22,15 @@ const reducer = (state, action) => {
   }
 
   if (action.type === "DECREASE") {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+    return { ...state, cart: tempCart };
   }
 
   return state;
