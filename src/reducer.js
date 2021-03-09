3fb35d4e9d7a5e18a46422fc462305c9919e177e
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-03-09 11:58:34 -0800

    Refactored increase + decrease into one function

--- src/reducer.js
+++ src/reducer.js
@@ -11,27 +11,27 @@ const reducer = (state, action) => {
     };
   }
 
-  if (action.type === "INCREASE") {
-    let tempCart = state.cart.map((cartItem) => {
-      if (cartItem.id === action.payload) {
-        return { ...cartItem, amount: cartItem.amount + 1 };
-      }
-      return cartItem;
-    });
-    return { ...state, cart: tempCart };
-  }
+  // if (action.type === "INCREASE") {
+  //   let tempCart = state.cart.map((cartItem) => {
+  //     if (cartItem.id === action.payload) {
+  //       return { ...cartItem, amount: cartItem.amount + 1 };
+  //     }
+  //     return cartItem;
+  //   });
+  //   return { ...state, cart: tempCart };
+  // }
 
-  if (action.type === "DECREASE") {
-    let tempCart = state.cart
-      .map((cartItem) => {
-        if (cartItem.id === action.payload) {
-          return { ...cartItem, amount: cartItem.amount - 1 };
-        }
-        return cartItem;
-      })
-      .filter((cartItem) => cartItem.amount !== 0);
-    return { ...state, cart: tempCart };
-  }
+  // if (action.type === "DECREASE") {
+  //   let tempCart = state.cart
+  //     .map((cartItem) => {
+  //       if (cartItem.id === action.payload) {
+  //         return { ...cartItem, amount: cartItem.amount - 1 };
+  //       }
+  //       return cartItem;
+  //     })
+  //     .filter((cartItem) => cartItem.amount !== 0);
+  //   return { ...state, cart: tempCart };
+  // }
 
   if (action.type === "GET_TOTAL") {
     let { total, amount } = state.cart.reduce(
@@ -59,6 +59,24 @@ const reducer = (state, action) => {
 
   if (action.type === "DISPLAY_ITEMS") {
     return { ...state, cart: action.payload, loading: false };
+  }
+
+  if (action.type === "TOGGLE_AMOUNT") {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          if (action.payload.type === "inc") {
+            return { ...cartItem, amount: cartItem.amount + 1 };
+          }
+          if (action.payload.type === "dec") {
+            return { ...cartItem, amount: cartItem.amount - 1 };
+          }
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+
+    return { ...state, cart: tempCart };
   }
 
   return state;
