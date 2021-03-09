e6cc9358447b4141d48c629d17a6ba03cec3c26e
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-03-08 16:52:21 -0800

    Increase is working

--- src/reducer.js
+++ src/reducer.js
@@ -11,6 +11,19 @@ const reducer = (state, action) => {
     };
   }
 
+  if (action.type === "INCREASE") {
+    let tempCart = state.cart.map((cartItem) => {
+      if (cartItem.id === action.payload) {
+        return { ...cartItem, amount: cartItem.amount + 1 };
+      }
+      return cartItem;
+    });
+    return { ...state, cart: tempCart };
+  }
+
+  if (action.type === "DECREASE") {
+  }
+
   return state;
 };
 
